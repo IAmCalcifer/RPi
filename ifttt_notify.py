c2630422a9e95ae7842df64b5ee0c4d4d41e2399
Tesselex-Solutions <55358331+Tesselex-Solutions@users.noreply.github.com>
2021-03-15 23:48:00 -0400

    Update and rename startup_notify.py to ifttt_notify.py

---
+++ ifttt_notify.py
@@ -0,0 +1,2 @@
+import requests
+requests.post('https://maker.ifttt.com/trigger/EVENT_NAME/with/key/SECRET_KEY_FROM_DOCUMENTATION', params={"value1":"none","value2":"none","value3":"none"})
--- startup_notify.py
+++
@@ -1,2 +0,0 @@
-import requests
-requests.post('https://maker.ifttt.com/trigger/rpi_startup/with/key/qAFGJxjt', params={"value1":"RPi","value2":"none","value3":"none"})
