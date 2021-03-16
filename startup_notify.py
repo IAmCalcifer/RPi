aad7431cece1ffda08c83496fb0aec82759eb0ea
Tesselex-Solutions <55358331+Tesselex-Solutions@users.noreply.github.com>
2021-03-15 22:28:15 -0400

    Update startup_notify.py

--- startup_notify.py
+++ startup_notify.py
@@ -1,1 +1,2 @@
- r = requests.post('https://maker.ifttt.com/trigger/rpi_startup/with/key/qAFGJxjt', params={"value1":"RPi","value2":"none","value3":"none"})
+import requests
+requests.post('https://maker.ifttt.com/trigger/rpi_startup/with/key/qAFGJxjt', params={"value1":"RPi","value2":"none","value3":"none"})
